14a04beff98b164acccdfe722d4e1192cbb714de
Felix Manuel <luxotus@gmail.com>
2018-07-02 19:15:35 -0400

    init project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LandingScreen from './components/landing-screen/index.jsx';
+
+ReactDOM.render(
+  <LandingScreen />,
+  document.getElementById('root'),
+);
