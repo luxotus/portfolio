baf27e37d2248c5d217fca4d8a332db098ef80bb
Felix Manuel <luxotus@gmail.com>
2018-07-02 22:37:31 -0400

    Added text sections on landing screen

---
+++ src/components/header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h4>
+          {'Felix Manuel'}
+        </h4>
+      </div>
+    );
+  }
+}
--- src/components/landing-screen/index.jsx
+++ src/components/landing-screen/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BackgroundParticles from '../background-particles/index.jsx';
+import Header from '../header/index.jsx';
 import './index.css';
 
 export default class LandingScreen extends React.Component {
@@ -11,6 +12,19 @@ export default class LandingScreen extends React.Component {
   render() {
     return (
       <div id="landing-screen">
+        <Header />
+        <div id="landing-text-holder">
+          <h1>
+            {'I create'}
+            <br />
+            {'interactive'}
+            <br />
+            {'experiences.'}
+          </h1>
+          <p>
+            {'Web developer that uses JavaScript and CSS to build high-end interactive sites, applications and tools.'}
+          </p>
+        </div>
         <BackgroundParticles />
       </div>
     );
