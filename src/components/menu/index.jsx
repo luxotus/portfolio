77727efe3be642a3cab2add7f0d75531369dcd38
Felix Manuel <luxotus@gmail.com>
2018-07-04 01:45:37 -0400

    Added icons and styled list elements, going to bed

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -2,10 +2,35 @@ import React from 'react';
 import './index.css';
 
 export default class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        'lab',
+        'blog',
+        'sites',
+        'contact',
+        'open source',
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="menu-container">
-
+        <ul>
+          {this.state.items.map(item => (
+            <li key={item}>
+              <a href="/{item}">
+                {item}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="icon-container">
+          <a href="https://github.com/luxotus" className="icon-github" />
+          <a href="https://codepen.io/luxotus/" className="icon-codepen" />
+        </div>
       </div>
     );
   }
