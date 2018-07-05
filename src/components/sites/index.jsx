17d0c0c67b9480695a76a44b23d1062b77db2da2
Felix Manuel <luxotus@gmail.com>
2018-07-04 23:11:40 -0400

    Sites container is showing items styled but still have to setup hover

--- src/components/sites/index.jsx
+++ src/components/sites/index.jsx
@@ -6,6 +6,7 @@ export default class Sites extends React.Component {
     super(props);
     this.state = {};
     this.subMenuItems = this.subMenuItems.bind(this);
+    this.siteItems = this.siteItems.bind(this);
   }
 
   subMenuItems() {
@@ -28,6 +29,72 @@ export default class Sites extends React.Component {
     );
   }
 
+  siteItems() {
+    const listSites = [
+      {
+        name: 'Main Home',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+      {
+        name: 'Simple Slide',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+      {
+        name: 'Team Gold',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+      {
+        name: 'Main Home2',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+      {
+        name: 'Simple Slide2',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+      {
+        name: 'Team Gold2',
+        types: [
+          'React',
+          'Sass',
+        ],
+        imageSrc: '/images/test.jpg',
+      },
+    ];
+
+    return (
+      listSites.map(item => (
+        <div className="sites-item" key={item.name}>
+          <div className="item-overlay">
+            {item.name}
+          </div>
+          <div className="item-image">
+            <img src={item.imageSrc} alt={item.name} />
+          </div>
+        </div>
+      ))
+    );
+  }
+
   render() {
     return (
       <div id="sites-container">
@@ -37,6 +104,9 @@ export default class Sites extends React.Component {
         <ul>
           {React.createElement(this.subMenuItems)}
         </ul>
+        <div className="sites-section">
+          {React.createElement(this.siteItems)}
+        </div>
       </div>
     );
   }
