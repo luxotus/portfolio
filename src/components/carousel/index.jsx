dbd81237160292f481523e4a6ca31837e8512137
Felix Manuel <luxotus@gmail.com>
2018-07-06 21:22:51 -0400

    added more structure to carousel

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -10,12 +10,29 @@ export default class Carousel extends React.Component {
   render() {
     return (
       <div id="carousel-container">
-        <div id="left-hidden-item" className="floating-item hidden-item left" />
-        <div id="left-shown-item" className="floating-item side-item left" />
-        <div id="middle-shown-item" className="floating-item main-item" />
-        <div id="right-shown-item" className="floating-item side-item right" />
-        <div id="right-hidden-item" className="floating-item hidden-item right" />
+        <div className="carousel">
+          <div id="left-hidden-item" className="floating-item hidden-item left" />
+          <div id="left-shown-item" className="floating-item side-item left" />
+          <div id="middle-shown-item" className="floating-item main-item">
+            <div id="details">
+              <h1>
+                {'Arkade'}
+                <br />
+                {'London'}
+              </h1>
+              <p>
+                {'- Audio Reactive Art'}
+              </p>
+            </div>
+          </div>
+          <div id="right-shown-item" className="floating-item side-item right" />
+          <div id="right-hidden-item" className="floating-item hidden-item right" />
+        </div>
+        <div id="bread-crumbs">
+          <div className="hexagon"></div>
+        </div>
       </div>
+
     );
   }
 }
