5554077240b6ae51cea058ad76c130df46102be2
Felix Manuel <luxotus@gmail.com>
2018-07-18 21:32:57 -0400

    Added some test data and logs as I setup the rotate slides function

--- src/components/home-page/lab/index.jsx
+++ src/components/home-page/lab/index.jsx
@@ -3,6 +3,76 @@ import { Link } from 'react-router-dom';
 import './index.css';
 
 export default class Lab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slides: {
+        leftHidden: 0,
+        leftVisible: 1,
+        middle: 2,
+        rightHidden: 3,
+        rightVisible: 4,
+      },
+      data: [
+        {
+          id: 0,
+          title: 'Garlic',
+          link: '/lab/garlic',
+        },
+        {
+          id: 1,
+          title: 'Salt',
+          link: '/lab/salt',
+        },
+        {
+          id: 2,
+          title: 'Rosemary',
+          link: '/lab/rosemary',
+        },
+        {
+          id: 3,
+          title: 'Basil',
+          link: '/lab/basil',
+        },
+        {
+          id: 4,
+          title: 'Olives',
+          link: '/lab/olives',
+        },
+      ],
+    };
+    this.rotateSlides = this.rotateSlides.bind(this);
+  }
+
+  rotateSlides(direction) {
+    const step = (direction === 'left') ? -1 : 1;
+    const updateSlides = {};
+    Object.entries(this.state.slides).forEach(([key, value]) => {
+      if (value <= 0) {
+        updateSlides[key] = this.state.data.length - 1;
+      } else {
+        updateSlides[key] = this.state.slides[key] + step;
+      }
+    });
+
+    console.log(updateSlides);
+
+
+    this.setState({ slides: updateSlides }, () => {
+      console.table(this.state.slides);
+    });
+
+    console.log(`Direction: ${direction}`);
+  }
+
+  /**
+   * Builds dots under the middle slide
+   * @param {{title: string, isActive: boolean}} items
+   */
+  dots(items) {
+    console.log(items);
+  }
+
   render() {
     return (
       <div id="lab-container">
@@ -38,8 +108,14 @@ export default class Lab extends React.Component {
           </div>
           <div className="bread-crumbs">
             <div className="dot-section">
-              <div className="dot" />
-              <div className="dot" />
+              <div
+                className="dot"
+                role="button"
+                tabIndex="0"
+                onClick={() => this.rotateSlides('left')}
+                onKeyDown={() => this.rotateSlides('left')}
+              />
+              <div className="dot active" />
               <div className="dot" />
             </div>
           </div>
