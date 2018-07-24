0b33539f6cbe9a79817c7ab2be58016909c1f1a3
Felix Manuel <luxotus@gmail.com>
2018-07-23 23:22:31 -0400

    Im loving this animation!!

--- src/components/home-page/blog/index.jsx
+++ src/components/home-page/blog/index.jsx
@@ -31,6 +31,7 @@ export default class Blog extends React.Component {
 
     this.keyCount = 0;
     this.getKey = this.getKey.bind(this);
+    this.updateBlogPost = this.updateBlogPost.bind(this);
   }
 
   getKey() {
@@ -40,7 +41,7 @@ export default class Blog extends React.Component {
 
   buildPreviewBlogPost(blogPost) {
     return (
-      <div>
+      <div className="blog-sides-wrapper">
         <div id="blog-left-section" className="blog-sections">
           <div className="pad">
             <h1>
@@ -66,8 +67,76 @@ export default class Blog extends React.Component {
             </Link>
           </div>
         </div>
+        <div id="blog-eraser" />
       </div>
     );
+  }
+
+  updateBlogPost(direction) {
+    let pos = 0;
+    const returnDirection = direction === 'left' ? 'right' : 'left';
+
+    if (direction === 'right') {
+      pos = (this.state.visibleBlogPost >= this.blogData.length - 1)
+        ? 0 : this.state.visibleBlogPost + 1;
+    } else {
+      pos = (this.state.visibleBlogPost <= 0)
+        ? this.blogData.length - 1 : this.state.visibleBlogPost - 1;
+    }
+
+    this.setState({ visibleBlogPost: pos }, () => {
+      this.eraserAnimation(returnDirection);
+    });
+  }
+
+  eraserAnimation(direction) {
+    const element = document.getElementById('blog-eraser');
+    const width = {
+      current: parseFloat(window.getComputedStyle(element, null).getPropertyValue('width')),
+      max: window.innerWidth,
+    };
+    const step = 5;
+    const milSec = 10;
+    const needsToBeUnErased = (width.current >= width.max);
+    element.style.display = 'block';
+
+    if (!needsToBeUnErased && direction === 'left') {
+      element.style.left = 0;
+      element.style.right = 'inherit';
+      element.style.borderRight = '5px solid aqua';
+      element.style.borderLeft = '';
+    }
+
+    if (!needsToBeUnErased && direction === 'right') {
+      element.style.left = 'inherit';
+      element.style.right = 0;
+      element.style.borderRight = '';
+      element.style.borderLeft = '5px solid aqua';
+    }
+
+    if (needsToBeUnErased) {
+      const timer = setInterval(() => {
+        if (width.current <= 0) {
+          clearInterval(timer);
+          element.style.width = '0px';
+          element.style.display = 'none';
+        } else {
+          element.style.width = `${width.current}px`;
+          width.current -= step;
+        }
+      }, milSec);
+    } else {
+      const timer = setInterval(() => {
+        if (width.current >= width.max) {
+          clearInterval(timer);
+          element.style.width = `${width.max}px`;
+          this.updateBlogPost(direction);
+        } else {
+          element.style.width = `${width.current}px`;
+          width.current += step;
+        }
+      }, milSec);
+    }
   }
 
   buildDots(acitveID) {
@@ -90,11 +159,21 @@ export default class Blog extends React.Component {
             </a>
           </h1>
         </div>
-        <div className="arrow-section left">
+        <div
+          className="arrow-section left"
+          role="presentation"
+          onClick={() => this.eraserAnimation('left')}
+          onKeyDown={() => this.eraserAnimation('left')}
+        >
           <div className="arrow" />
         </div>
         {this.buildPreviewBlogPost(this.blogData[this.state.visibleBlogPost])}
-        <div className="arrow-section right">
+        <div
+          className="arrow-section right"
+          role="presentation"
+          onClick={() => this.eraserAnimation('right')}
+          onKeyDown={() => this.eraserAnimation('right')}
+        >
           <div className="arrow" />
         </div>
         <div className="bread-crumbs">
