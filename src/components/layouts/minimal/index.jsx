cc9dbfc1289d6d9a8c14e224a34231f916d00fa3
Felix Manuel <luxotus@gmail.com>
2018-07-29 12:03:35 -0400

    bread crumbs, test data

--- src/components/layouts/minimal/index.jsx
+++ src/components/layouts/minimal/index.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import './index.css';
 
 export default class Minimal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeID: 0,
+    };
+
+    this.keyCount = 0;
+    this.getKey = this.getKey.bind(this);
+    this.breadCrumbs = this.breadCrumbs.bind(this);
+  }
+
+  getKey() {
+    this.keyCount += 1;
+    return this.keyCount;
+  }
+
+  breadCrumbs() {
+    return this.props.data.map(crumb => (
+      <li key={this.getKey()} data-id={crumb.id}>
+        <div className="bread-crumb" />
+        <div className={(crumb.id === this.state.activeID) ? 'crumb-line active' : 'crumb-line'} />
+      </li>
+    ));
+  }
+
   render() {
     return (
       <div id="minimal-layout-container">
         <div className="bread-crumbs-holder">
           <ul>
-            <li>
-              <div className="bread-crumb" />
-              <div className="crumb-line" />
-            </li>
-            <li>
-              <div className="bread-crumb" />
-            </li>
-            <li>
-              <div className="bread-crumb" />
-            </li>
+            {this.breadCrumbs()}
           </ul>
         </div>
         <div className="content-holder">
--- src/components/other-pages/lab/index.jsx
+++ src/components/other-pages/lab/index.jsx
@@ -3,9 +3,42 @@ import './index.css';
 import Minimal from '../../layouts/minimal';
 
 export default class LabPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.labData = [
+      {
+        id: 0,
+        title: 'Crooked Smile',
+        images: {
+          sm: '/images/land.jpg',
+          md: '/images/land.jpg',
+          lg: '/images/land.jpg',
+        },
+      },
+      {
+        id: 1,
+        title: 'Forbidden Fruit',
+        images: {
+          sm: '/images/land.jpg',
+          md: '/images/land.jpg',
+          lg: '/images/land.jpg',
+        },
+      },
+      {
+        id: 2,
+        title: 'Born Sinner',
+        images: {
+          sm: '/images/land.jpg',
+          md: '/images/land.jpg',
+          lg: '/images/land.jpg',
+        },
+      },
+    ];
+  }
+
   render() {
     return (
-      <Minimal />
+      <Minimal data={this.labData} />
     );
   }
 }
