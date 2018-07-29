128ad7cdb780ba4c046f141a784a2f9d23c29135
Felix Manuel <luxotus@gmail.com>
2018-07-28 22:24:04 -0400

    setting up page layouts

---
+++ src/components/layouts/minimal/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './index.css';
+
+export default class Minimal extends React.Component {
+  render() {
+    return (
+      <div id="minimal-layout-container">
+        <div className="bread-crumbs-holder">
+          <ul>
+            <li>
+              <div className="bread-crumb" />
+              <div className="crumb-line" />
+            </li>
+            <li>
+              <div className="bread-crumb" />
+            </li>
+            <li>
+              <div className="bread-crumb" />
+            </li>
+          </ul>
+        </div>
+        <div className="content-holder">
+          <section />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/other-pages/lab/index.jsx
+++ src/components/other-pages/lab/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
+import Minimal from '../../layouts/minimal';
 
 export default class LabPage extends React.Component {
   render() {
     return (
-      <div id="lab-page-container" />
+      <Minimal />
     );
   }
 }
