102f66e7e1aaed9561d6aebfadf99dd2184b5717
Felix Manuel <luxotus@gmail.com>
2018-07-30 23:27:58 -0400

    will be scrapping layout since desired scrolling behaviour lack consistency across browsers

--- src/components/layouts/minimal/index.jsx
+++ src/components/layouts/minimal/index.jsx
@@ -8,10 +8,14 @@ export default class Minimal extends React.Component {
     this.state = {
       activeID: 0,
     };
+    this.crumbAnimating = false;
+    this.scrollTimer = 0;
+    this.lastScrollFireTime = 0;
 
     this.keyCount = 0;
     this.getKey = this.getKey.bind(this);
     this.breadCrumbs = this.breadCrumbs.bind(this);
+    this.trackScroll = this.trackScroll.bind(this);
   }
 
   getKey() {
@@ -53,12 +57,23 @@ export default class Minimal extends React.Component {
     ));
   }
 
+  breadCrumbWithoutAnimation(index) {
+    document.querySelector('.bread-crumbs-holder li .crumb-line.active').classList.remove('active');
+    document.querySelector(`.bread-crumbs-holder li[data-id="${index}"] .crumb-line`).classList.add('active');
+    this.setState({ activeID: index });
+  }
+
   breadCrumbAnimation(index) {
+    if (!this.crumbAnimating) {
+      this.crumbAnimating = true;
+    }
+
     const targetEl = document.querySelector(`.bread-crumbs-holder li[data-id="${index}"] .crumb-line`);
     const activeEl = document.querySelector('.bread-crumbs-holder li .crumb-line.active');
     const lineWidth = parseInt(window.getComputedStyle(activeEl).getPropertyValue('width'), 10);
     const step = 10;
     let i = lineWidth;
+
     const disappear = setInterval(() => {
       i -= step;
       activeEl.style.width = `${i}px`;
@@ -73,6 +88,8 @@ export default class Minimal extends React.Component {
             clearInterval(reappear);
             activeEl.classList.remove('active');
             targetEl.classList.add('active');
+            this.crumbAnimating = false;
+            this.setState({ activeID: index });
           }
         }, 20);
       }
@@ -94,18 +111,48 @@ export default class Minimal extends React.Component {
         i += step;
         if (i >= scrollPos) {
           clearInterval(interval);
-          this.setState({ activeID: index });
         }
       } else {
         i -= step;
         if (i <= scrollPos) {
           clearInterval(interval);
-          this.setState({ activeID: index });
         }
       }
     }, 20);
 
     this.breadCrumbAnimation(index);
+  }
+
+  trackScroll() {
+    const holderScrollTop = document.querySelector('.content-holder').scrollTop;
+    const wrapperOffsets = [].slice.call(document.querySelectorAll('.content-wrapper')).map(el => el.offsetTop);
+    const distances = wrapperOffsets.map(offset => Math.abs(holderScrollTop - offset));
+    const currentBreadcrumb = distances.indexOf(Math.min(...distances));
+
+    if (currentBreadcrumb !== this.state.activeID) {
+      this.breadCrumbWithoutAnimation(currentBreadcrumb);
+    }
+  }
+
+  scrollingEvent() {
+    if (this.crumbAnimating) {
+      return;
+    }
+
+    const minScrollTime = 100;
+    const now = new Date().getTime();
+
+    if (!this.scrollTimer) {
+      if (now - this.lastScrollFireTime > (3 * minScrollTime)) {
+        this.trackScroll();
+        this.lastScrollFireTime = now;
+      }
+      this.scrollTimer = setTimeout(() => {
+        this.scrollTimer = null;
+        this.lastScrollFireTime = new Date().getTime();
+        this.trackScroll();
+      }, minScrollTime);
+    }
   }
 
   render() {
@@ -116,7 +163,10 @@ export default class Minimal extends React.Component {
             {this.breadCrumbs()}
           </ul>
         </div>
-        <div className="content-holder">
+        <div
+          className="content-holder"
+          onScroll={() => this.scrollingEvent()}
+        >
           {this.content()}
         </div>
       </div>
