2369652851f932e34305093bfd6c31f6f0b5775f
Felix Manuel <luxotus@gmail.com>
2018-08-01 21:26:45 -0400

    just copied test data to sites page since its layout will be the same just different content

--- src/components/other-pages/sites/index.jsx
+++ src/components/other-pages/sites/index.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import './index.css';
+import Minimal from '../../layouts/minimal';
 
 export default class SitesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.siteData = [
+      {
+        id: 0,
+        title: 'Crooked Smile',
+        link: '/lab/crooked-smile',
+        images: {
+          sm: '/images/site_01.png',
+          md: '/images/site_01.png',
+          lg: '/images/site_01.png',
+        },
+      },
+      {
+        id: 1,
+        title: 'Forbidden Fruit',
+        link: '/lab/forbidden-fruit',
+        images: {
+          sm: '/images/site_02.png',
+          md: '/images/site_02.png',
+          lg: '/images/site_02.png',
+        },
+      },
+      {
+        id: 2,
+        title: 'Born Sinner',
+        link: '/lab/born-sinner',
+        images: {
+          sm: '/images/site_03.png',
+          md: '/images/site_03.png',
+          lg: '/images/site_03.png',
+        },
+      },
+      {
+        id: 3,
+        title: 'French Toast',
+        link: '/lab/born-sinner',
+        images: {
+          sm: '/images/site_04.png',
+          md: '/images/site_04.png',
+          lg: '/images/site_04.png',
+        },
+      },
+    ];
+  }
+
   render() {
     return (
-      <div id="sites-page-container" />
+      <Minimal data={this.siteData} />
     );
   }
 }
