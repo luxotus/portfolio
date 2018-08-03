dd01dd3715583ace1210e056748d157fa5206dcc
Felix Manuel <luxotus@gmail.com>
2018-08-02 21:31:08 -0400

    Added css for when js is added to expand image and to show full blog content.

--- src/components/layouts/maximum/index.jsx
+++ src/components/layouts/maximum/index.jsx
@@ -20,19 +20,24 @@ export default class Maximum extends React.Component {
       <div id="maximum-layout-container">
         <div className="content-holder">
           <div className="blog-image" style={{ backgroundImage: 'url(/images/blog-lg-01.jpg)' }} />
-          <div className="blog-content">
-            <h1>
-              {'The Great Debate: Vanilla JS or JQuery'}
-            </h1>
-            <div className="description-holder">
-              <p>
-                {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.'}
-              </p>
+          <div className="blog-holder">
+            <div className="blog-wrapper">
+              <h1>
+                {'The Great Debate: Vanilla JS or JQuery'}
+              </h1>
+              <div className="blog-content">
+                <div className="description">
+                  {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.'}
+                </div>
+                <div className="content">
+                  {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.'}
+                </div>
+              </div>
+              <button type="button">
+                {'Read More'}
+                <div />
+              </button>
             </div>
-            <button type="button">
-              {'Read More'}
-              <div />
-            </button>
           </div>
         </div>
       </div>
