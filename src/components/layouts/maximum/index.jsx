3629c0cf4c774ce664729c803e8a931db63a81ce
Felix Manuel <luxotus@gmail.com>
2018-08-04 16:20:28 -0400

    refactored jsx to seperate functions

--- src/components/layouts/maximum/index.jsx
+++ src/components/layouts/maximum/index.jsx
@@ -3,54 +3,75 @@ import React from 'react';
 import './index.css';
 
 export default class Maximum extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeID: 0,
+    };
+    this.data = [
+      {
+        title: 'The Great Debate: Vanilla JS or JQuery',
+        image: '/images/blog-lg-01.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.',
+        content: 'Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus.Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus',
+      },
+    ];
+  }
 
-  //   this.keyCount = 0;
-  //   this.getKey = this.getKey.bind(this);
-  // }
+  createBlogElement(data) {
+    return (
+      <div className="blog-wrapper">
+        <h1>
+          {data.title}
+        </h1>
+        <div className="blog-content">
+          <div className="description">
+            {data.description}
+          </div>
+          <div className="content">
+            {data.content}
+          </div>
+        </div>
+        <button type="button" className="read-more">
+          {'Read More'}
+          <div />
+        </button>
+      </div>
+    );
+  }
 
-  // getKey() {
-  //   this.keyCount += 1;
-  //   return this.keyCount;
-  // }
+  createActionBtns() {
+    return (
+      <div className="arrow-container">
+        <button type="button" className="left-arrow arrow">
+          <div />
+        </button>
+        <button type="button" className="right-arrow arrow">
+          <div />
+        </button>
+        <button type="button" className="close">
+          <div className="x" />
+        </button>
+      </div>
+    );
+  }
+
+  createContentHolder(data) {
+    return (
+      <div className="content-holder">
+        <div className="blog-image min" style={{ backgroundImage: `url(${data.image})` }} />
+        <div className="blog-holder">
+          {this.createBlogElement(data)}
+          {this.createActionBtns()}
+        </div>
+      </div>
+    );
+  }
 
   render() {
     return (
       <div id="maximum-layout-container">
-        <div className="content-holder">
-          <div className="blog-image" style={{ backgroundImage: 'url(/images/blog-lg-01.jpg)' }} />
-          <div className="blog-holder">
-            <div className="blog-wrapper">
-              <h1>
-                {'The Great Debate: Vanilla JS or JQuery'}
-              </h1>
-              <div className="blog-content">
-                <div className="description">
-                  {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.'}
-                </div>
-                <div className="content">
-                  {'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet luctus neque, at tincidunt odio. Phasellus sit amet pulvinar risus, sit amet ultrices felis. Vestibulum accumsan neque in metus sodales ultrices. Morbi iaculis viverra nunc ac sodales. Donec vitae tempor tortor. Vivamus eget magna quis magna lacinia pharetra.'}
-                </div>
-              </div>
-              <button type="button" className="read-more">
-                {'Read More'}
-                <div />
-              </button>
-              <div className="arrow-container">
-                <button type="button" className="left-arrow arrow">
-                  <div />
-                </button>
-                <button type="button" className="right-arrow arrow">
-                  <div />
-                </button>
-                <button type="button" className="close">
-                  <div className="x" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        {this.createContentHolder(this.data[this.state.activeID])}
       </div>
     );
   }
