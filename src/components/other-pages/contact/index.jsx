4fe486257808ba297c8e91c3b99b8fa3dd33fc81
Felix Manuel <luxotus@gmail.com>
2018-08-07 19:55:39 -0400

    added simple contact page

--- src/components/other-pages/contact/index.jsx
+++ src/components/other-pages/contact/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './index.css';
+import Contact from '../../home-page/contact';
 
 export default class ContactPage extends React.Component {
   render() {
     return (
-      <div id="contact-page-container" />
+      <div id="contact-page-container">
+        <Contact />
+      </div>
     );
   }
 }
