0817d98574885f2c66c76bd2c1f9f9490577194c
Felix Manuel <luxotus@gmail.com>
2018-08-24 22:12:42 -0400

    fix for link path in blog

--- src/components/home-page/blog/index.jsx
+++ src/components/home-page/blog/index.jsx
@@ -5,6 +5,7 @@ import './index.css';
 export default class Blog extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       visibleBlogPost: 0,
     };
@@ -12,13 +13,13 @@ export default class Blog extends React.Component {
       {
         title: 'Minimum Viable Product',
         description: 'When your starting a new product you should have a minimal viable product. Without it you could end up with a solution to a problem that no one uses or cares about. With it you can reduce your overall risk and increase your chances of launching a successful product.',
-        link: '/blog/test',
+        link: '/blog',
         image: '/images/blog/minimum-viable-product-side.jpeg',
       },
       {
         title: 'Keeping up with the latest in your Dev Community',
         description: 'Keeping up with the latest in your development community will help you grow as a developer. From finding better employment opportunities to gaining insight into some of your common problems that others in the community have solved.',
-        link: '/blog/test',
+        link: '/blog',
         image: '/images/blog/keeping-up-with-the-latest.jpeg',
       },
     ];
