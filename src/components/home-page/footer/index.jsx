2a434d4ce906cfb6a7edfc4146ec34122650aa66
luxotus <luxotus@gmail.com>
2019-12-10 16:53:03 -0600

    added internal page links to footer

--- src/components/home-page/footer/index.jsx
+++ src/components/home-page/footer/index.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.css';
 
 export default class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.pageLinks = [
+      {
+        title: 'home',
+        link: '/',
+      },
+      {
+        title: 'sites',
+        link: '/sites',
+      },
+      {
+        title: 'blog',
+        link: '/blog',
+      },
+      {
+        title: 'contact',
+        link: '/contact',
+      },
+    ];
+    this.keyCount = 0;
+    this.getKey = this.getKey.bind(this);
+  }
+
+  getKey() {
+    this.keyCount += 1;
+    return this.keyCount;
+  }
+
   render() {
     return (
       <footer>
@@ -11,6 +42,15 @@ export default class Footer extends React.Component {
             © 2019
           </span>
         </h2>
+        <ul>
+          {this.pageLinks.map((item) => (
+            <li key={`footer-links-${this.getKey()}`} className="">
+              <Link to={item.link}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
         <div className="icon-container">
           <a href="https://github.com/luxotus" className="icon-github" aria-label="github" />
           <a href="https://twitter.com/ManuelF_Dev" className="icon-twitter" aria-label="twitter" />
