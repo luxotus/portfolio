376441e52739f9f338c4446b6f819277537b440e
luxotus <luxotus@gmail.com>
2019-12-12 16:56:31 -0600

    added portal holders for above fold on landing page

--- src/components/home-page/landing-screen.jsx
+++ src/components/home-page/landing-screen.jsx
@@ -1,71 +1,31 @@
 import React from 'react';
-import BackgroundParticles from './background-particles';
 
 export default class LandingScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showParticles: true,
-    };
-    this.data = {
-      color: {
-        general: '#a9a9a9',
-        points: '#000000',
-        line: '#bdbdbd',
-      },
-      number: {
-        value: 10,
-      },
-    };
-
-    this.updateParticleState = this.updateParticleState.bind(this);
-  }
-
-  detectWhenSectionNotVisible(selector) {
-    const el = document.querySelector(selector);
-    const { showParticles } = this.state;
-
-    if (el !== null) {
-      const height = parseInt(window.getComputedStyle(el).height, 10);
-      const topBufferSpace = 50;
-
-      if (showParticles && window.scrollY + topBufferSpace > height) {
-        this.updateParticleState(false);
-      } else if (!showParticles && window.scrollY + topBufferSpace < height) {
-        this.updateParticleState(true);
-      }
-    }
-  }
-
-  updateParticleState(show) {
-    this.setState({ showParticles: show });
-  }
-
-  addParticles() {
-    return (
-      <BackgroundParticles data={this.data} />
-    );
-  }
-
   render() {
-    const { showParticles } = this.state;
-
     return (
-      <div id="landing-screen">
-        <div id="landing-text-holder">
-          <h1>
-            I Create
-            <br />
-            Interactive
-            <br />
-            Experiences.
-          </h1>
-          <p>
-            Web developer that uses JavaScript and CSS to build high-end interactive sites,
-            applications and tools.
-          </p>
+      <div className="landing-screen-wrapper">
+        <div className="text-flex-section">
+          <div className="text-grid-section">
+            <h1>
+              I Create
+              <br />
+              Interactive
+              <br />
+              Experiences.
+            </h1>
+            <p>
+              Web developer that uses JavaScript and CSS to build high-end interactive sites,
+              applications and tools.
+            </p>
+          </div>
         </div>
-        {showParticles ? this.addParticles() : ''}
+        <div className="visual-section">
+          <div className="lg-diamond" />
+          <div className="sm-diamond top-left" />
+          {/* <div className="sm-diamond top-right" /> */}
+          <div className="sm-diamond bottom-left" />
+          <div className="sm-diamond bottom-right" />
+        </div>
       </div>
     );
   }
