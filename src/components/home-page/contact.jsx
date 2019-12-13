6248838f5dacf64bb7533ab554428ff513d25fbb
luxotus <luxotus@gmail.com>
2019-12-12 20:06:19 -0600

    working on new contact section

--- src/components/home-page/contact.jsx
+++ src/components/home-page/contact.jsx
@@ -1,75 +1,59 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import BackgroundParticles from './background-particles';
 
 export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showParticles: true,
-    };
-    this.data = {
-      color: {
-        general: '#a9a9a9',
-        points: '#000000',
-        line: '#bdbdbd',
-      },
-      number: {
-        value: 10,
-      },
-    };
-
-    this.updateParticleState = this.updateParticleState.bind(this);
-  }
-
-  detectWhenSectionNotVisible(selector) {
-    const el = document.getElementById(selector);
-    const { showParticles } = this.state;
-
-    if (el !== null) {
-      const height = parseInt(window.getComputedStyle(el).height, 10);
-      const buffer = 300;
-      const offsetTop = document.getElementById(selector).offsetTop - (height + buffer);
-
-      if (showParticles && window.scrollY < offsetTop) {
-        this.updateParticleState(false);
-      } else if (!showParticles && window.scrollY > offsetTop) {
-        this.updateParticleState(true);
-      }
-    }
-  }
-
-  updateParticleState(show) {
-    this.setState({ showParticles: show });
-  }
-
-  addParticles() {
-    return (
-      <BackgroundParticles data={this.data} />
-    );
-  }
-
   render() {
-    const { showParticles } = this.state;
-
     return (
-      <div id="contact-container">
-        <div className="title">
-          <h2>
-            <Link to="/contact">
-              Get in Touch
-            </Link>
-          </h2>
-          <p>
-            Any feedback or inquiries is much appreciated!
-          </p>
+      <div className="contact-wrapper">
+        <div className="inner-wrapper left">
+          <h2>Let&apos;s get in touch</h2>
+          <form>
+            <div className="group">
+              <label htmlFor="contact-name">Name</label>
+              <input type="text" name="name" id="contact-name" />
+            </div>
+
+            <div className="group">
+              <label htmlFor="contact-email">Email</label>
+              <input type="email" name="email" id="contact-email" />
+            </div>
+
+            <div className="group">
+              <label htmlFor="contact-subject">Subject</label>
+              <input type="text" name="subject" id="contact-subject" />
+            </div>
+
+            <div className="group active">
+              <label htmlFor="contact-message">Message</label>
+              <textarea name="" id="contact-message" cols="30" rows="10" />
+            </div>
+          </form>
         </div>
-        <h1>
-          <a href="mailto:hello@felixmanuel.com">
-            hello@felixmanuel.com
-          </a>
-        </h1>
-        {showParticles ? this.addParticles() : ''}
+        <div className="inner-wrapper right">
+          <div className="item">
+            <h4>Location</h4>
+            <p>
+              301 Cannan Dr,
+              <br />
+              Angleton, TX
+              <br />
+              77515
+            </p>
+          </div>
+          <div className="item">
+            <h4>Available Hours</h4>
+            <p>
+              301 Cannan Dr,
+              <br />
+              Angleton, TX
+              <br />
+              77515
+            </p>
+          </div>
+          <div className="item">
+            <h4>Contact</h4>
+            <p>hello@felixmanuel.com</p>
+          </div>
+        </div>
       </div>
     );
   }
